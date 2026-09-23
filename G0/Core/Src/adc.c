fb5936896b21c2631485a2f976c7c2c7be699288
Tanner Downing-McAdams <20tandown@gmail.com>
2026-09-23 10:47:15 -0700

    Previous G0 ADC sampling require 6.4ms, which does not fit in the new 1ms window. 160.5 samples is overkill for decoupled potentiometers, so 12.5 is used with 256x oversample retained.

--- G0/Core/Src/adc.c
+++ G0/Core/Src/adc.c
@@ -58,8 +58,8 @@ void MX_ADC1_Init(void)
   hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
   hadc1.Init.DMAContinuousRequests = ENABLE;
   hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
-  hadc1.Init.SamplingTimeCommon1 = ADC_SAMPLETIME_160CYCLES_5;
-  hadc1.Init.SamplingTimeCommon2 = ADC_SAMPLETIME_160CYCLES_5;
+  hadc1.Init.SamplingTimeCommon1 = ADC_SAMPLETIME_12CYCLES_5;
+  hadc1.Init.SamplingTimeCommon2 = ADC_SAMPLETIME_12CYCLES_5;
   hadc1.Init.OversamplingMode = ENABLE;
   hadc1.Init.Oversampling.Ratio = ADC_OVERSAMPLING_RATIO_256;
   hadc1.Init.Oversampling.RightBitShift = ADC_RIGHTBITSHIFT_8;
